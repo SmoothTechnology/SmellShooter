561df22aaf38cda1f7ebde4b5e7b3a893cc75751
jmzjmzjmz <hi.im.vito@gmail.com>
2017-07-17 22:04:51 -0400

    added defines to indicate which teensy/pump ID, removed temperature, sends on different channels

--- DreamMachine/DreamMachine.ino
+++ DreamMachine/DreamMachine.ino
@@ -10,16 +10,90 @@
 //////////////////// BEGIN OPTIONS ///////////////
 ///////////////////////////////////////////////////
 #define LEDPIN 17
-int led_height = 75;
 
-#define SPARKLE_COLOR strip.Color(255,123,0)
+#define SPARKLE_COLOR strip.Color(100,100,100)
+// #define SPARKLE_COLOR strip.Color(255,123,0)
 #define MAX_PRESSURE_COLOR strip.Color(0,random(255),random(255))
-#define MAX_INFLATE_COLOR 20
-#define MIN_INFLATE_COLOR 0
+// #define MAX_INFLATE_COLOR 20
+// #define MIN_INFLATE_COLOR 0
+
+// SET LED HEIGHT, COLOR, AND CHANGE USB NAME
+#define ID_3
+
+#ifdef ID_1
+    //1
+    #define MAX_INFLATE_COLOR myColor(50,100,100)
+    #define MIN_INFLATE_COLOR myColor(20,50,25)
+    int led_height = 54;
+    #define MIDI_CHANNEL 1
+#endif    
+#ifdef ID_2
+    //2
+    #define MAX_INFLATE_COLOR myColor(70,120,20)
+    #define MIN_INFLATE_COLOR myColor(30,50,20)
+    int led_height = 60;
+    #define MIDI_CHANNEL 2
+#endif    
+#ifdef ID_3
+    //3
+    #define MAX_INFLATE_COLOR myColor(0,100,0)
+    #define MIN_INFLATE_COLOR myColor(0,50,0)
+    int led_height = 61;
+    #define MIDI_CHANNEL 3
+#endif    
+#ifdef ID_4
+    //4
+    #define MAX_INFLATE_COLOR myColor(150,0,0)
+    #define MIN_INFLATE_COLOR myColor(50,0,0)
+    int led_height = 65;
+    #define MIDI_CHANNEL 4
+#endif    
+#ifdef ID_5
+    //5
+    #define MAX_INFLATE_COLOR myColor(120,30,0)
+    #define MIN_INFLATE_COLOR myColor(30,10,0)
+    int led_height = 70;
+    #define MIDI_CHANNEL 5
+#endif    
+#ifdef ID_6
+    //6
+    #define MAX_INFLATE_COLOR myColor(40,15,0)
+    #define MIN_INFLATE_COLOR myColor(40,15,0)
+    int led_height = 70;
+    #define MIDI_CHANNEL 6
+#endif    
+#ifdef ID_7
+    // 7 - B HOPE
+    #define MAX_INFLATE_COLOR myColor(125,65,0)
+    #define MIN_INFLATE_COLOR myColor(80,40,0)
+    int led_height = 75;
+    #define MIDI_CHANNEL 7
+#endif    
+#ifdef ID_8
+    //8
+    #define MAX_INFLATE_COLOR myColor(155,0,85)
+    #define MIN_INFLATE_COLOR myColor(80,0,60)
+    int led_height = 76;
+    #define MIDI_CHANNEL 8
+#endif    
+#ifdef ID_9
+    //9
+    #define MAX_INFLATE_COLOR myColor(125,60,85)
+    #define MIN_INFLATE_COLOR myColor(80,40,60)
+    int led_height = 93;
+    #define MIDI_CHANNEL 9
+#endif    
+#ifdef ID_10
+    //10
+    #define MAX_INFLATE_COLOR myColor(0,0,150)
+    #define MIN_INFLATE_COLOR myColor(30,30,70)
+    int led_height = 94;
+    #define MIDI_CHANNEL 10
+#endif
 
 // Control Pressure Inflate and Deflate
-int threshold = 2000; // Pressure must exceed this value to inflate
-int decrementVal = 3000; // Speed of pressure deflate
+int threshold = 500; // Pressure must exceed this value to inflate
+int decrementVal = 2500; // Speed of pressure deflate
 ///////////////////////////////////////////////////
 //////////////////// END OPTIONS //////////////////
 ///////////////////////////////////////////////////
@@ -29,10 +103,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(led_height * 4, LEDPIN, NEO_GRB + NE
 MPL3115A2 myPressure;
 
 #define POTPIN A0
-#define MIDI_CHANNEL 1
 #define CC_PRESSURE 1
-#define CC_TEMPERATURE 2
-#define CC_INTEGRAL 3
+#define CC_INTEGRAL 2
 
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
@@ -117,12 +189,11 @@ unsigned long prevTime = 0;
 void loop() {
 
   midiPressure = getScaledPressure(setIntegral);
-  midiTemperature = getScaledTemperature();
 
    //fill to scaled height
-   //colorFill(strip.Color(255,0,0), midiPressure);
-
-   colorFill(Wheel(map(setIntegral, 0, 127, MAX_INFLATE_COLOR, MIN_INFLATE_COLOR)), setIntegral);
+   float percentFilled = float( map(setIntegral, 0, 127, 0, 100) ) / 100.0;
+   Serial.println(percentFilled);
+   colorFill(lerpColor(MIN_INFLATE_COLOR,MAX_INFLATE_COLOR , percentFilled), setIntegral);
 
    if(setIntegral == 127)
    {
@@ -144,10 +215,6 @@ void loop() {
     prevIntegral = setIntegral;
   }
 
-  if (midiTemperature != prevTemp) {
-      usbMIDI.sendControlChange(CC_TEMPERATURE, midiTemperature, MIDI_CHANNEL);
-      prevTemp = midiTemperature;
-    }
 
   while (usbMIDI.read()) {
   }
@@ -196,18 +263,6 @@ int getScaledPressure(int &integral){
 
 }
 
-int getScaledTemperature(){
-
-  float tempF = myPressure.readTempF();
-  int minTemp = 65;
-  int maxTemp = 95;
-
-  if(tempF < minTemp)  tempF = minTemp;
-  if(tempF > maxTemp)  tempF = maxTemp;
-
-  return map(tempF, minTemp, maxTemp, 0, 127);
-}
-
 void setLEDColor(int i, uint32_t c) {
     strip.setPixelColor(i, c);    //turn every third pixel on
     strip.setPixelColor(led_height * 2 - i - 1, c);    //turn every third pixel on
@@ -237,4 +292,28 @@ void sparkleSpecial() {
     strip.show();
 }
 
+
+uint8_t red(uint32_t c) {
+  return ((c & 0xFF0000) >> 16);
+}
+uint8_t green(uint32_t c) {
+  return ((c & 0x00FF00) >> 8);
+}
+uint8_t blue(uint32_t c) {
+  return (c & 0x0000FF);
+}
+
+byte lerp(byte a, byte b, float t) {
+  return a + (b - a)*t; 
+}
+
+uint32_t myColor(uint8_t r , uint8_t g , uint8_t b){
+  return ((uint32_t)(r) << 16) | ((uint32_t)(g ) <<  8) | (b );
+}
+
+uint32_t lerpColor(uint32_t a, uint32_t b, float t) {
+  return myColor(lerp(red(a), red(b), t), 
+  lerp(green(a), green(b), t), 
+  lerp(blue(a), blue(b), t));
+}
 
